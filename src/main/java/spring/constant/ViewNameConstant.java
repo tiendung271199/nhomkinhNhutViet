dc0764ae9b823d48f2a10cba6fd104193ddbbedd
Tronghien98 <andrew.hien98@gmail.com>
2021-07-25 17:01:10 +0700

    tách ghép giao diện public

---
+++ src/main/java/spring/constant/ViewNameConstant.java
@@ -0,0 +1,15 @@
+package spring.constant;
+
+public class ViewNameConstant {
+	
+	public static final String INDEX = "nhomkinhnhutviet.index";
+	public static final String NEWS = "nhomkinhnhutviet.news";
+	public static final String DETAIL = "nhomkinhnhutviet.detail";
+	public static final String CONTACT = "nhomkinhnhutviet.contact";
+	public static final String ABOUT = "nhomkinhnhutviet.about";
+
+	public static final String ERROR_404 = "views/error/404";
+	public static final String ERROR_403 = "views/error/403";
+	public static final String ERROR_400 = "views/error/400";
+	
+}
---
+++ src/main/webapp/WEB-INF/views/views/demo/demo.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ include file="/WEB-INF/views/components/taglib.jsp"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Demo</title>
+</head>
+
+<body>
+	<h2>View Demo</h2>
+</body>
+</html>
